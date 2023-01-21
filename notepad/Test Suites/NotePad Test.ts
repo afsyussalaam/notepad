e48e23b77480a98a7c462083e2f1669d7833eeda
Rizky Afsyus Salaam <122716670+afsyussalaam@users.noreply.github.com>
2023-01-21 18:43:27 +0700

    Notepad first commit

---
+++ notepad/Test Suites/NotePad Test.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>NotePad Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6c3be12e-c874-42ce-9d6b-4d359ff769ad</testSuiteGuid>
+</TestSuiteEntity>
